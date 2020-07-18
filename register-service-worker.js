c6c36fa4e22b7cbe6cfdb57529823d01ad8f855a
Yahya Bagia <yahya.bagia@hotmail.com>
2020-07-18 15:13:46 +0530

    Updates

---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/tic-tac-toe-expo/expo-service-worker.js",{scope:"/tic-tac-toe-expo/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
