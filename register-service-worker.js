62ae51139555f78d083aa278a58c3bfeb6146557
YahyaBagia <yahya.bagia@hotmail.com>
2021-02-17 12:17:03 +0530

    Updates

--- register-service-worker.js
+++
@@ -1,1 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/TicTacToe-Expo/expo-service-worker.js",{scope:"/TicTacToe-Expo/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
